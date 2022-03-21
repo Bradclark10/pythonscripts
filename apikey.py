a0a8c183851ce65c98b5e12446c898762cc03abe
Bradleyclark98 <bradsytchannel@gmail.com>
2022-03-21 20:46:00 +0000

    Delete apikey.py

--- apikey.py
+++
@@ -1,6 +0,0 @@
-import datetime
-current_time = datetime.datetime.now(datetime.timezone.utc)
-current_time = current_time.timestamp() # works if Python >= 3.3
-
-new_end_date = current_time + (43800*3* 60)  # 5 min * 60 seconds
-print (new_end_date)
